b47aa6aef24c7ff183a44db0ade59822a0d65153
jason99chang <46538983+jason99chang@users.noreply.github.com>
2020-08-05 04:16:56 -0700

    made landing pagea and navbar

--- scroll.js
+++
@@ -1,6 +0,0 @@
-const parallax = document.getElementById("parallax");
-
-window.addEventListener("scroll", function(){
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPoisitionY = offset * 0.7 + "px"
-})
